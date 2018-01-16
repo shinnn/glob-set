d4a9f17727c68566552708d64fd37ecbdd587612
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-01-16 16:38:14 +0900

    update dependencies and devDependencies
    Highlights:
    
    * switch from istanbul to nyc https://github.com/istanbuljs/nyc
    * update @shinnn/eslint-config and follow its coding rules, for exmaple tab indent

--- index.js
+++ index.js
@@ -6,52 +6,52 @@ const inspectWithKind = require('inspect-with-kind');
 const validateGlobOpts = require('validate-glob-opts');
 
 module.exports = function globSet(...args) {
-  const argLen = args.length;
+	const argLen = args.length;
 
-  if (argLen !== 1 && argLen !== 2) {
-    return Promise.reject(new RangeError(`Expected 1 or 2 arguments (<string>[, <Object>]), but got ${
-      argLen === 0 ? 'no' : argLen
-    } arguments instead.`));
-  }
+	if (argLen !== 1 && argLen !== 2) {
+		return Promise.reject(new RangeError(`Expected 1 or 2 arguments (<string>[, <Object>]), but got ${
+			argLen === 0 ? 'no' : argLen
+		} arguments instead.`));
+	}
 
-  const [pattern, options] = args;
+	const [pattern, options] = args;
 
-  if (typeof pattern !== 'string') {
-    return Promise.reject(new TypeError(`Expected a glob pattern (string), but got a non-string value ${
-      inspectWithKind(pattern)
-    }.`));
-  }
+	if (typeof pattern !== 'string') {
+		return Promise.reject(new TypeError(`Expected a glob pattern (string), but got a non-string value ${
+			inspectWithKind(pattern)
+		}.`));
+	}
 
-  if (argLen === 2) {
-    const validationResults = validateGlobOpts(options);
+	if (argLen === 2) {
+		const validationResults = validateGlobOpts(options);
 
-    if (validationResults.length !== 0) {
-      return Promise.reject(new GlobOptionError(validationResults));
-    }
-  }
+		if (validationResults.length !== 0) {
+			return Promise.reject(new GlobOptionError(validationResults));
+		}
+	}
 
-  let resolve;
-  let reject;
+	let resolve;
+	let reject;
 
-  const promise = new Promise((resolveArg, rejectArg) => {
-    resolve = resolveArg;
-    reject = rejectArg;
-  });
+	const promise = new Promise((resolveArg, rejectArg) => {
+		resolve = resolveArg;
+		reject = rejectArg;
+	});
 
-  const glob = new Glob(pattern, Object.assign({
-    silent: true,
-    strict: true
-  }, options), (err, found) => {
-    if (err) {
-      reject(err);
-      return;
-    }
+	const glob = new Glob(pattern, Object.assign({
+		silent: true,
+		strict: true
+	}, options), (err, found) => {
+		if (err) {
+			reject(err);
+			return;
+		}
 
-    resolve(new Set(found));
-  });
+		resolve(new Set(found));
+	});
 
-  glob.then = promise.then.bind(promise);
-  glob.catch = promise.catch.bind(promise);
+	glob.then = promise.then.bind(promise);
+	glob.catch = promise.catch.bind(promise);
 
-  return glob;
+	return glob;
 };
--- test.js
+++ test.js
@@ -5,52 +5,52 @@ const test = require('tape');
 const unglobbable = require('unglobbable');
 
 test('globSet()', t => {
-  t.plan(8);
+	t.plan(8);
 
-  globSet('*.js').then(found => {
-    t.equal(found.size, 2, 'should be resolved with a Set instance.');
-    t.deepEqual([...found], ['index.js', 'test.js'], 'should search files with a glob pattern.');
-  });
+	globSet('*.js').then(found => {
+		t.equal(found.size, 2, 'should be resolved with a Set instance.');
+		t.deepEqual([...found], ['index.js', 'test.js'], 'should search files with a glob pattern.');
+	});
 
-  globSet('{,*/}*', {nobrace: true}).then(({size}) => {
-    t.equal(size, 0, 'should support node-glob options.');
-  });
+	globSet('{,*/}*', {nobrace: true}).then(({size}) => {
+		t.equal(size, 0, 'should support node-glob options.');
+	});
 
-  const fail = t.fail.bind(t, 'Glob unexpectedly succeeded.');
+	const fail = t.fail.bind(t, 'Glob unexpectedly succeeded.');
 
-  globSet(unglobbable, {}).then(fail, ({code}) => {
-    t.equal(code[0], 'E', 'should fail when node-glob emits an error.');
-  });
+	globSet(unglobbable, {}).then(fail, ({code}) => {
+		t.equal(code[0], 'E', 'should fail when node-glob emits an error.');
+	});
 
-  globSet(new Uint8Array()).then(fail, ({message}) => {
-    t.equal(
-      message,
-      'Expected a glob pattern (string), but got a non-string value Uint8Array [  ].',
-      'should fail when the first argument is not a string.'
-    );
-  });
+	globSet(new Uint8Array()).then(fail, ({message}) => {
+		t.equal(
+			message,
+			'Expected a glob pattern (string), but got a non-string value Uint8Array [  ].',
+			'should fail when the first argument is not a string.'
+		);
+	});
 
-  globSet('pattern', {realPathCache: true}).then(fail, ({message}) => {
-    t.equal(
-      message,
-      'node-glob doesn\'t have `realPathCache` option. Probably you meant `realpathCache`.',
-      'should fail when it takes invalid glob option.'
-    );
-  });
+	globSet('pattern', {realPathCache: true}).then(fail, ({message}) => {
+		t.equal(
+			message,
+			'node-glob doesn\'t have `realPathCache` option. Probably you meant `realpathCache`.',
+			'should fail when it takes invalid glob option.'
+		);
+	});
 
-  globSet().then(fail, err => {
-    t.equal(
-      err.toString(),
-      'RangeError: Expected 1 or 2 arguments (<string>[, <Object>]), but got no arguments instead.',
-      'should fail when it takes no arguments.'
-    );
-  });
+	globSet().then(fail, err => {
+		t.equal(
+			err.toString(),
+			'RangeError: Expected 1 or 2 arguments (<string>[, <Object>]), but got no arguments instead.',
+			'should fail when it takes no arguments.'
+		);
+	});
 
-  globSet('1', '2', '3').then(fail, err => {
-    t.strictEqual(
-      err.toString(),
-      'RangeError: Expected 1 or 2 arguments (<string>[, <Object>]), but got 3 arguments instead.',
-      'should fail when it takes too many arguments.'
-    );
-  });
+	globSet('1', '2', '3').then(fail, err => {
+		t.strictEqual(
+			err.toString(),
+			'RangeError: Expected 1 or 2 arguments (<string>[, <Object>]), but got 3 arguments instead.',
+			'should fail when it takes too many arguments.'
+		);
+	});
 });
